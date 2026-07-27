cf58baffc0cc8357570de5f6ada8a4c7674eea59
SikandarJODD <sikandar6174@gmail.com>
2026-07-28 02:09:54 +0530

    feat: added new example

--- src/lib/examples/examples.ts
+++ src/lib/examples/examples.ts
@@ -2,6 +2,7 @@ import type { Component } from "svelte";
 import Basic from "./basic.svelte";
 import { Simple } from ".";
 import Vertical from "./vertical.svelte";
+import VerticalParallel from "./vertical-parallel.svelte";
 
 type Example = {
     name: string;
@@ -20,5 +21,9 @@ export const examples: Example[] = [
     {
         name: "Vertical",
         component: Vertical
+    },
+    {
+        name: "Vertical with Parallel Nodes",
+        component: VerticalParallel,
     }
 ]
